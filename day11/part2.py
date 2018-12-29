bc64599f8fbaeceac5e5b9cc75b13ecfa9508d04
Andrew Webb <andrew@awebb.info>
2018-12-29 14:03:15 +0000

    Day 11 part 2, significantly faster

--- day11/part2.py
+++ day11/part2.py
@@ -1,28 +1,35 @@
-from collections import Counter
-
-with open('input') as f:
-    step_list = f.read().strip().split(',')
+from collections import defaultdict
 
 dirs = ['n', 'ne', 'se', 's', 'sw', 'nw']
-opposites = [(dirs[i], dirs[i + 3]) for i in range(3)]
-reduction_triples = [(dirs[i], dirs[(i + 2)%6], dirs[(i + 1)%6]) for i in range(6)]
+
+with open('input') as f:
+    step_list = [dirs.index(d) for d in f.read().strip().split(',')]
 
 def compute_reduction(step_list):
-    steps = Counter(step_list)
+    steps = defaultdict(int)
 
-    for dir1, dir2 in opposites:
-        reduction = min(steps[dir1], steps[dir2])
-        steps[dir1] -= reduction
-        steps[dir2] -= reduction
+    dists = []
 
-    for dir1, dir2, dir3 in reduction_triples:
-        reduction = min(steps[dir1], steps[dir2])
-        steps[dir1] -= reduction
-        steps[dir2] -= reduction
-        steps[dir3] += reduction
+    for step in step_list:
+        opposite = (step + 3) % 6
+        plus1 = (step + 1) % 6
+        plus2 = (step + 2) % 6
+        minus1 = (step - 1) % 6
+        minus2 = (step - 2) % 6
+        if steps[opposite] > 1:
+            steps[opposite] -= 1
+        elif steps[plus2] > 0:
+            steps[plus2] -= 1
+            steps[plus1] += 1
+        elif steps[minus2] > 0:
+            steps[minus2] -= 1
+            steps[minus1] += 1
+        else:
+            steps[step] += 1
 
-    return sum(steps.values())
+        dists.append(sum(steps.values()))
 
-dists = [compute_reduction(step_list[:i]) for i in range(1, len(step_list) + 1, 1)]
+    return max(dists)
 
-print(max(dists))
+ans = compute_reduction(step_list)
+print(ans)
