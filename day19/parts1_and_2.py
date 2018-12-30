87c213277e4535fff0a1dcf7ad26e182e20b8165
Andrew Webb <andrew@awebb.info>
2018-12-30 23:31:00 +0000

    Tidied up day 19 solution

--- day19/parts1_and_2.py
+++ day19/parts1_and_2.py
@@ -1,46 +1,34 @@
 with open('input') as f:
     data = f.read().splitlines()
 
-HORIZONTAL = 0
-VERTICAL = 1
-
-y = 0
+# Initial position
 x = data[0].index('|')
+y = 0
 
-orientation = VERTICAL
-direction = 1
+# Initial velocity
+dx = 0
+dy = 1
 
 letters_seen = ''
 steps = 0
 
-while True:
-    if orientation == VERTICAL:
-        y += direction
-    else:
-        x += direction
-
+while data[y][x] != ' ':
+    # Step forwards
+    x += dx
+    y += dy
     steps += 1
-    c = data[y][x]
 
-    if c == ' ':
-        break
-
-    if c.isalpha():
-        letters_seen += c
-
-    if c == '+':
-        if orientation == VERTICAL:
-            orientation = HORIZONTAL
-            for delta in [-1, 1]:
-                if data[y][x + delta] != ' ':
-                    direction = delta
-                    break
-        else:
-            orientation = VERTICAL
-            for delta in [-1, 1]:
-                if data[y + delta][x] != ' ':
-                    direction = delta
-                    break
+    # If we see a letter, add it to the sequence seen
+    if data[y][x].isalpha():
+        letters_seen += data[y][x]
+    # If we're at a corner
+    elif data[y][x] == '+':
+        # Look for a direction we can step that isn't where we just came from
+        for dx1, dy1 in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            if data[y + dy1][x + dx1] != ' ' and not (dx1 == -dx and dy1 == -dy):
+                dx = dx1
+                dy = dy1
+                break
 
 print(f'Letters seen: {letters_seen}')
 print(f'{steps} steps taken')
